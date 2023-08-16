703ea5674dd8e88dac3c00884e52be813b018dff
Mateusz Baryla <m.baryla@protonmail.ch>
2023-08-16 14:42:39 +0200

    Part of authentification added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/FakeAuthContext";
+import Button from "../components/Button";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      /*We use replace: true in order to come back pushing the 
+      turn button. It's important to make the browser work as usual. */
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +53,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
